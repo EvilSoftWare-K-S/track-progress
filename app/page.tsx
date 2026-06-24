47330ce360cfcbe9e6979bb157320a7cbba1ec7c
Sergey Krahmalyov <sergey.krahmaliov@gmail.com>
2026-06-24 23:41:36 +0400

    feat:init/choosing basic styles

---
+++ app/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import {ArrowRight} from 'lucide-react'
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col bb-white">
+      <main className=" flex-1">
+        <section className=" container mx-auto px-4 py-32">
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold">
+              The best way to track the progress of something.
+            </h1>
+            <p className="text-muted-foreground mb-19 text-xl">
+              Capture, organize, and manage your progress in one place.
+            </p>
+            <div className="flex flex-col items-center gap-4">
+              <Button size='lg' className='h-12 px-8 text-lg font-medium'>Start for free <ArrowRight className="ml-2"/></Button>
+              <p className="text-sm text-mutted-foreground">Free forever. No credit card required.</p>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
